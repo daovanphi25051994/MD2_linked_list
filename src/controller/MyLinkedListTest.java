d02ab1082230547d36800fd2a16e0c0d3f3a6b8d
phi <daovanphi25051994@gmail.com>
2020-04-02 17:12:48 +0700

    ok

--- src/controller/MyLinkedListTest.java
+++ src/controller/MyLinkedListTest.java
@@ -2,6 +2,7 @@ package controller;
 
 import linkedlist.MyLinkedList;
 import model.Student;
+import org.w3c.dom.Node;
 
 public class MyLinkedListTest {
     public static void main(String[] args) {
@@ -9,14 +10,16 @@ public class MyLinkedListTest {
         Student student1 = new Student("dao", 18);
         Student student2 = new Student("van", 19);
         Student student3 = new Student("phi", 20);
+        Student student4 = new Student("dao van phi", 21);
+        linkedListStudent.add(student1);
+        linkedListStudent.add(student2);
+        linkedListStudent.add(student3);
+        linkedListStudent.add(student4);
 
-        linkedListStudent.addFirst(student1);
-        linkedListStudent.addLast(student2);
-        linkedListStudent.addLast(student3);
-        linkedListStudent.remove(student1);
-
-        linkedListStudent.printList();
-        System.out.println(linkedListStudent.size());
+        Object[] students = linkedListStudent.clone();
+        for (Object student : students) {
+            System.out.println(student.toString());
+        }
 
     }
 }
--- src/linkedlist/MyLinkedList.java
+++ src/linkedlist/MyLinkedList.java
@@ -17,7 +17,6 @@ public class MyLinkedList<E> {
     public void add(int index, E element) {
         Node temp = head;
         Node holder;
-
         for (int i = 0; i < index - 1 && temp.next != null; i++) {
             temp = temp.next;
         }
@@ -28,17 +27,28 @@ public class MyLinkedList<E> {
     }
 
     public void addFirst(E element) {
-        Node temp = head;
-        head = new Node(element);
-        head.next = temp;
-        tail = head;
-        numNodes++;
+        if (numNodes == 0) {
+            head = new Node(element);
+            tail = head;
+            numNodes++;
+        } else {
+            Node temp = head;
+            head = new Node(element);
+            head.next = temp;
+            numNodes++;
+        }
     }
 
     public void addLast(E element) {
-        tail.next = new Node(element);
-        tail = tail.next;
-        numNodes++;
+        if (numNodes == 0) {
+            head = new Node(element);
+            tail = head;
+            numNodes++;
+        } else {
+            tail.next = new Node(element);
+            tail = tail.next;
+            numNodes++;
+        }
     }
 
 
@@ -53,12 +63,10 @@ public class MyLinkedList<E> {
     public E remove(int index) {
         Node beforeElement = head;
         Node holder;
-
         for (int i = 0; i < index - 1 && beforeElement.next != null; i++) {
             beforeElement = beforeElement.next;
         }
         holder = beforeElement.next;
-
         beforeElement.next = holder.next;
         numNodes--;
         return (E) holder;
@@ -67,9 +75,13 @@ public class MyLinkedList<E> {
     public boolean remove(E element) {
         Node step = head;
         while (step.next != null) {
-            if (step.getData().equals(element)) {
-                Node deleteElement = step.next.next;
-                step.next = deleteElement;
+            if (head.data.equals(element)) {
+                head = head.next;
+                numNodes--;
+                return true;
+            }
+            if (step.next.data.equals(element)) {
+                step.next = step.next.next;
                 numNodes--;
                 return true;
             }
@@ -77,18 +89,91 @@ public class MyLinkedList<E> {
         }
         return false;
     }
-    public int size(){
+
+    public int size() {
         return numNodes;
+    }
+
+    public boolean contains(E element) {
+        Node temp = head;
+        while (temp.next != null) {
+            if (temp.next.data.equals(element)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public int indexOf(E element) {
+        Node temp = head;
+        int index = 1;
+        while (temp.next != null) {
+            if (head.data.equals(element)) {
+                return 0;
+            }
+            if (temp.next.data.equals(element)) {
+                return index;
+            }
+            temp = temp.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean add(E element) {
+        if (numNodes == 0) {
+            head = new Node(element);
+            tail = head;
+            numNodes++;
+            return true;
+        } else if (numNodes > 0) {
+            tail.next = new Node(element);
+            tail = tail.next;
+            numNodes++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public E get(int index) {
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return (E) temp.data;
+    }
+
+    public E getFirst() {
+        return (E) head.data;
+    }
+
+    public E getLast() {
+        return (E) tail.data;
+    }
+
+    public void clear() {
+        this.head = null;
+        this.tail = null;
+        numNodes = 0;
+    }
+    public E[] clone(){
+        Object[] newList = new Object[numNodes];
+        Node temp = head;
+       for (int i = 0; i < numNodes; i++){
+            newList[i] = (E) temp.data;
+            temp = temp.next;
+       }
+       return (E[]) newList;
     }
 
     private class Node {
         private Node next;
         private Object data;
-
         public Node(Object data) {
             this.data = data;
         }
-
         public Object getData() {
             return this.data;
         }
